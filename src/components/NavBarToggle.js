28f3291d78b592ea1a74d7866886d85dbf0c543b
raphaelconstantino <raphaelc23@gmail.com>
2017-02-12 18:15:34 -0200

    Add event on filter for mobile

--- src/components/NavBarToggle.js
+++ src/components/NavBarToggle.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 export default class NavBarToggle extends Component {
-	render () {
+	
+    navToggle (e) {
+        let bodyClass = document.querySelector('html').classList;
+        if (bodyClass.contains("nav-open"))
+        {
+            bodyClass.remove("nav-open");
+        } else {
+            bodyClass.add("nav-open");
+        }            
+        
+    }
+    
+    render () {
 		return (
-            <button type="button" className="navbar-toggle" data-toggle="collapse">
+            <button onClick={this.navToggle} type="button" className="navbar-toggle" data-toggle="collapse">
                 <span className="sr-only">Toggle navigation</span>
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
