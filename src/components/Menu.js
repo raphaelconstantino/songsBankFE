f6fc342567f54fc8fcd339b70b15790377cfd65c
Raphael <raphaelc23@gmail.com>
2017-02-14 07:54:18 -0200

    Activate correct menu when refreshing

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,11 +9,47 @@ export default class Menu extends Component {
     this.clickMenu = this.clickMenu.bind(this);
 	}
 
-	clickMenu = (text, icon) => {
+	clickMenu = (pathName) => {
      return function () {
-      this.setState({text: text, icon : icon});
+        this.mapIcons(pathName);
      }.bind(this)
-	};  
+	};
+
+  mapIcons (pathName) {
+    if (pathName === "dashboard")
+    {
+      this.setState({text: "Dashboard", icon : "dashboard"});
+    }
+
+    if (pathName === "songs")
+    {
+      this.setState({text: "Songs", icon : "music_note"});
+    }
+
+    if (pathName === "genders")
+    {
+      this.setState({text: "Genders", icon : "style"});
+    }
+ 
+    if (pathName === "instruments")
+    {
+      this.setState({text: "Instruments", icon : "content_paste"});
+    }
+  }
+
+  componentWillMount () {
+    var pathName = this.props.pathName.split("/");
+    
+    if (pathName.length < 1) {
+      return;  
+    }
+
+    pathName = pathName[1];
+
+    this.mapIcons(pathName);
+
+  }
+  
 
   render() {
     
@@ -29,16 +65,16 @@ export default class Menu extends Component {
         <div className="sidebar-wrapper">
           <div className="nav-container">
             <ul className="nav">
-                <NavLink onClick={this.clickMenu("Dashboard", "dashboard")} to="/dashboard">
+                <NavLink onClick={this.clickMenu("dashboard")} to="/dashboard">
                   <i className="material-icons">dashboard</i> <p>Dashboard</p>
                 </NavLink>
-                <NavLink onClick={this.clickMenu("Songs", "music_note")} to="/songs">
+                <NavLink onClick={this.clickMenu("songs")} to="/songs">
                   <i className="material-icons">music_note</i> <p>Songs</p>
                 </NavLink>
-                <NavLink onClick={this.clickMenu("Genders", "style")} to="/genders">                
+                <NavLink onClick={this.clickMenu("genders")} to="/genders">                
                   <i className="material-icons">style</i><p>Genders</p>
                 </NavLink>
-                <NavLink onClick={this.clickMenu("Instruments", "content_paste")}  to="/instruments">                
+                <NavLink onClick={this.clickMenu("instruments")}  to="/instruments">                
                   <i className="material-icons">content_paste</i><p>Instruments</p>
                 </NavLink>
             </ul>
